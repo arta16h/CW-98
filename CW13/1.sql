883d0ebc1947f60f93328bc729173aa7975203fb
arta16h <135044355+arta16h@users.noreply.github.com>
2023-06-22 15:03:36 +0330

    Add files via upload

--- CW13/1.sql
+++ CW13/1.sql
@@ -12,9 +12,17 @@ join category on category.category_id = film_category.category_id
 WHERE category.name IN ('Action', 'Comedy', 'Family');
 
 --3
-SELECT category.name, COUNT(film. film_id) as count
+SELECT category.name, COUNT(film. film_id)
 FROM film
 join film_category on film_category.film_id = film.film_id
-join category on category.category_id = film_category.category_id
+join category on category.category_id = film_category.category_id;
 
 --4
+SELECT category.name, COUNT(film. film_id)
+FROM film
+join film_category on film_category.film_id = film.film_id
+join category on category.category_id = film_category.category_id 
+GROUP BY category.name
+having 60 < COUNT(film. film_id) < 68;
+
+--5

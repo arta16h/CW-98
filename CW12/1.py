aaeb1a90d2e692c44bfa40ac6923f72cc954d4e7
arta16h <135044355+arta16h@users.noreply.github.com>
2023-06-23 16:26:27 +0330

    Add files via upload

--- CW12/1.py
+++ CW12/1.py
@@ -4,11 +4,20 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 to_do_list = []
 
 class ToDoRequestHandler(BaseHTTPRequestHandler) :
-    def set_headers(self, status_code = 200, content_type = "text/plain") :
-        pass
+    def _set_headers(self, status_code = 200, content_type = "text/plain") :
+        self.send_response(status_code)
+        self.send_header("Content-type", content_type)
+        self.end_headers()
+
 
     def do_get(self) :
-        pass
+        if self.path == "/" :
+            self._set_headers(200, "application/json")
+            self.wfile.write(json.dumps(to_do_list).encode())
+        else:
+            self._set_headers(404)
+            self.wfile.write("Not Found".encode())
+            
 
     def do_post(self) :
         pass

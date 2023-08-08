9d5b98510c2a18a573d50828ba31793657500e9e
arta16h <sevda.hayati2015@gmail.com>
2023-08-08 19:48:55 +0330

    Fix: added args and kwargs

--- Debugging-Exercise/Todo Project/Home/mixins.py
+++ Debugging-Exercise/Todo Project/Home/mixins.py
@@ -2,23 +2,26 @@ from django.shortcuts import render, redirect
 from django.core.exceptions import PermissionDenied
 from .models import Todo
 from .forms import TodoForm
+from django.views import View
 
 
 class TodoMixin:
+    model = Todo
     form_class = TodoForm
     template_name = "Home/todo_detail.html"
 
     def dispatch(self, request, *args, **kwargs):
-        todo = Todo.objects.get(id=kwargs['id'])
+        todo = Todo.objects.get(id=kwargs['pk'])
         if not todo.user == request.user:
             raise PermissionDenied
+        return super().dispatch(request, *args, **kwargs)
 
-    def get(self, request, id):
-        todo = Todo.objects.filter(id=id)
+    def get(self, request, *args, **kwargs):
+        todo = Todo.objects.get(kwargs['pk'])
         return render(request, self.template_name, {'todo': todo})
 
-    def post(self, request, id):
-        todo = Todo.objects.get(id=id)
+    def post(self, request, *args, **kwargs):
+        todo = Todo.objects.get(kwargs['pk'])
         form = self.form_class(request.POST, instance=todo)
         if form.is_valid():
             todo.save()

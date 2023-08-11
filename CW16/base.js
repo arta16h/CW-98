51deadf67c98eea7c67158b5c59f0edee701cf0e
arta16h <sevda.hayati2015@gmail.com>
2023-08-11 16:21:16 +0330

    Chore: Task 7 description

--- CW16/base.js
+++ CW16/base.js
@@ -89,5 +89,9 @@ let strProfile = JSON.stringify(profile)
 localStorage.setItem('profile', strProfile)
 alert(localStorage.getItem('profile'))
 
+// 7
+/* Create a student object. The student object will have first name, last name,
+age, skills, country, enrolled keys and values for the keys. Store the student
+object in your browser localStorage. */
 
 

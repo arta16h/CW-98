33e1bccf004484229dc968a9217c439eb03b8cd7
Arta <sevda.hayati2015@gmail.com>
2023-08-17 15:53:54 +0330

    Refactor: categories url

--- HW14+/tasks/urls.py
+++ HW14+/tasks/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("search/", views.search, name="search"),
     path("task/<int:task_id>/", views.TaskDetailView.as_view(), name="task"),
     path("tasks/", views.task_page, name="task_page"),
-    path("categories/", views.all_categories, name="all_categories"),
+    path("categories/", views.CategoriesView, name="all_categories"),
     path("create_category/", views.createcategory, name="create_category"),
     path("create_task/", views.createtask, name="create_task")
     ]

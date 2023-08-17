966a694504eb5224af0c4179d1a3603fd452bf98
Arta <sevda.hayati2015@gmail.com>
2023-08-17 16:01:56 +0330

    Refactor: home url

--- HW14+/tasks/urls.py
+++ HW14+/tasks/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("", views.HomeView.as_view(), name="home"),
     path("search/", views.search, name="search"),
     path("task/<int:task_id>/", views.TaskDetailView.as_view(), name="task"),
     path("tasks/", views.task_page, name="task_page"),
-    path("categories/", views.CategoriesView, name="all_categories"),
+    path("categories/", views.CategoriesView.as_view(), name="all_categories"),
     path("create_category/", views.createcategory, name="create_category"),
     path("create_task/", views.createtask, name="create_task")
     ]
